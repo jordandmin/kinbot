c70ebe98a2da72098b4e69514c367f97584aa8be
MarlBurroW <marlburrow@gmail.com>
2026-02-26 09:43:58 +0000

    i18n: translate 'Channels' to 'Canaux' in FR, fix 'Francais' typo

--- src/client/pages/invite/InvitePage.tsx
+++ src/client/pages/invite/InvitePage.tsx
@@ -341,7 +341,7 @@ export function InvitePage() {
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="en">English</SelectItem>
-                  <SelectItem value="fr">Francais</SelectItem>
+                  <SelectItem value="fr">Français</SelectItem>
                 </SelectContent>
               </Select>
             </div>
